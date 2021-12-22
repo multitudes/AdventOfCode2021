fce7deb68032141e9be475f8dc530448d9427186
laurent brusa <wrmultitudes@gmail.com>
2021-12-22 14:14:33 +0100

    day8

--- AdventOfCode21/Day 8 - /Day8.swift
+++ AdventOfCode21/Day 8 - /Day8.swift
@@ -7,13 +7,30 @@
 
 import Foundation
 
+
+/// I removed the newlines in file input-8a manually, as said on the website, it was only done on the example to format the text on the page but the puzzle input had no such things
 func day8() {
-    let input: [Int] = getInputDay8()
+    let input: [(a: String, b: String)] = getInputDay8()
     print(input)
+    let solutionDay8a = input.map { $0.1 }
+        .reduce(0) { (acc, val) in
+            //print($1)
+            let components = val // $1 was a string
+                .trimmingCharacters(in: .whitespaces)
+                .components(separatedBy: .whitespaces)
+            print(components) // - gets to be an array
+            let partial = components.reduce(0) {
+                print($1)
+                if $1.count == 2 || $1.count == 3 || $1.count == 4 || $1.count == 7 {
+                    return $0 + 1
+                }
+                return $0
+            }
+            print("partial \(partial)")
+            return acc + partial
+        }
     
-    
-    
-    var solutionDay8a = 0
+
     var solutionDay8b = 0
     
     print("Solution day8 - Part1: \(solutionDay8a)")
@@ -21,23 +38,27 @@ func day8() {
 }
 
 
-func getInputDay8() -> [Int] {
-    var input: [Int] = []
+func getInputDay8() -> [(String,String)] {
+    var input: [(String,String)] = [(a: "", b:"")]
     do {
         let currentDirectory = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
         let bundleURL = URL(fileURLWithPath: "ResourceBundle.bundle", relativeTo: currentDirectory)
         let bundle = Bundle(url: bundleURL)
-        if let inputFileURL = bundle?.url(forResource: "input-8a", withExtension: "txt") {
+        if let inputFileURL = bundle?.url(forResource: "input-8", withExtension: "txt") {
             let charSet = CharacterSet(charactersIn: "|")
             do {
                 input = try String(contentsOf: inputFileURL)
-                    .components(separatedBy: .newlines )
+                    .trimmingCharacters(in: .newlines)
+                    .components(separatedBy: .newlines)
+                
                     .map {
-                        print("===")
-                        print($0)
-                      //  print($0.trimmingCharacters(in: charSet))
-                        //.getTrimmedCapturedGroupsFrom(regexPattern: "(\\d+),(\\d+) -> (\\d+),(\\d+)") ?? []
-                       return 0 }
+                        let splitString = $0.components(separatedBy: "|" )
+                        print(splitString[0])
+                        print(splitString[1])
+                        let (a,b) : (a: String,b: String) = (a:splitString[0],b: splitString[1])
+                        return (a:a,b:b)
+                        }
+              
             } catch {
                 print(error.localizedDescription)
             }
